fd87be2a449a1bd186bdead23c1f02a03d58cc0f
Muhammad Musa bashir <62591960+Musabashir143@users.noreply.github.com>
2021-11-05 13:12:44 +0500

    Add files via upload

---
+++ Password encrpter/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username, password, stationid;
+            string enc_password;
+            username = Console.ReadLine();
+            password = Console.ReadLine();
+            stationid = Console.ReadLine();
+
+            enc_password = Convert.ToBase64String(Encoding.UTF8.GetBytes(password));
+
+            string path = @"C:\Users\STARIZ.PK\source\repos\k180267\K180267_Q1\PASSWORD.txt"; //change the path
+
+            string clientHeader = username + "," + enc_password + "," + stationid + Environment.NewLine;
+
+            
+            if (!File.Exists(path))
+            {
+                using FileStream fs = File.Create(path);
+                File.WriteAllText(path, clientHeader);
+            }
+
+            File.AppendAllText(path, clientHeader + Environment.NewLine);
+
+        }
+    }
+}
